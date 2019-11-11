6286c63d710b16b2d91b7d65ea51bc771db0b41c
Jeremy <jeremy@uranusbytes.com>
2019-11-11 16:18:27 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,102 @@
+# update_guardduty_threat-list.py
+
+## Table of Contents
+* [Description](#description)
+* [Configuration](#Configuration)
+* [Troubleshooting](#troubleshooting)
+* [License](#license)
+
+## Description
+This script will download a copy of a threatlist (or multiple), reformat it, upload to an S3 bucket, and then configure/refresh AWS GuardDuty to use the threatlist.  
+
+This script has gone thru basic testing sufficient for my environment (CentOS 7 w/ Python 3.6 and OTX threatlist) and usage.  
+
+This script assumes that GuardDuty has been enabled and has detectors configured in all regions that threatlists will be uploaded to, as well as assumes there are other means for collection/alerting of findings.
+
+## Execution
+Normally you would have this script as a cron/scheduled job to execute on some frequency to update the guardduty threatlists.  FWIW, the open OTX is updated every 6hrs.  More frequent updates could have unknown implications to GuardDuty
+
+## Configuration
+All configuration for the script is done thru the Constants defined at the top of the file, starting around line 35, under the section named `Constants`
+### _THREAT_LISTS
+This is where threat lists are defined.  The default config is to use AlienVault's OTX.
+```
+_THREATLISTS = [
+  {
+    'list_name': 'OTX_Reputation',
+    'list_url': 'https://reputation.alienvault.com/reputation.generic.gz',
+    'list_format': 'TXT'
+  }
+]
+```
+### THREATLIST_S3_BUCKET
+This is the S3 bucket name that the threatlist will be uploaded to.
+
+`_THREATLIST_S3_BUCKET = 'myBucket'`
+This would upload the file to `https://s3.amazonaws.com/myBucket/myKeyPath/OTX_Reputation.txt`
+
+### _THREATLIST_S3_KEY_PATH
+This is the key path (without a pre or post slash) that will be used in the S3 bucket to store the threatlist.
+
+`_THREATLIST_S3_KEY_PATH = 'myKeyPath'`
+This would upload the file to `https://s3.amazonaws.com/myBucket/myKeyPath/OTX_Reputation.txt`
+
+### AWS_REGIONS
+This is a list that defines the regions that the threatlist will be configured for.  To define specific regions, the format would be:
+`_AWS_REGIONS = ['us-east-1', 'us-east-2']`
+
+To configure for all regions supported by guardduty, the format would be:
+`_AWS_REGIONS = boto3.session.get_available_regions(service_name='guardduty', partition_name='aws')`
+
+## Required IAM Permissions
+The following IAM permissions are required by the IAM user/role used to execute the script.  If the S3 bucket is cross account, if the S3 bucket is using KMS, or different configurations may require different permissions. 
+
+**Note**: Change the ServiceRole ARN to the appropriate service role being used for GuardDuty
+
+```
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "s3:PutObject"
+            ],
+            "Resource": [
+                "arn:aws:s3:::myBucketName/myKeyPath/*"
+            ]
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "guardduty:ListDetectors",
+                "guardduty:ListThreatIntelSets",
+                "guardduty:UpdateThreatIntelSet"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "iam:PutRolePolicy",
+                "iam:DeleteRolePolicy"
+            ],
+            "Resource": "arn:aws:iam::123412341234:role/aws-service-role/guardduty.amazonaws.com/AWSServiceRoleForAmazonGuardDuty"
+        }
+    ]
+}
+```
+
+## Troubleshooting
+There are two constants defined at the top of the plugin that can be used to facilitate debugging
+
+`_STDERR_OUTPUT_LEVEL` Change to `logging.INFO` for verbose output to stderr (same as `-v/-verbose`) or to `logging.DEBUG` for debug output to stderr (same as `-vv/--debug`)
+
+`_PRINT_STACKTRACE_ON_ERROR` Change to `True` to have the python stacktrace output to stderr when an error occurs
+
+## ToDo
+* Consider other threatlists
+
+## License
+All content is GPLv2
+
---
+++ update_guardduty_threat-list.py
@@ -0,0 +1,228 @@
+#!/usr/bin/env python
+""" Download the latest OTX reputation list, upload to S3, and update GuardDuty to use it
+
+https://github.com/UranusBytes/icinga-plugins
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+__author__ = "Jeremy Phillips"
+__contact__ = "jeremy@uranusbytes.com"
+__license__ = "GPLv2"
+__version__ = "0.1.0"
+
+import sys
+import requests
+from io import BytesIO
+import gzip
+import signal
+from datetime import datetime
+import logging
+import boto3
+import traceback
+
+# Constants
+###############################################################################
+_STDERR_OUTPUT_LEVEL = logging.CRITICAL  # Leave at logging.CRITICAL unless doing debugging
+_PRINT_STACKTRACE_ON_ERROR = False  # Show stacktrace to stderr on error
+# Learn about GuardDuty threat lists https://docs.aws.amazon.com/guardduty/latest/ug/guardduty_upload_lists.html
+_THREATLISTS = [
+  {
+    'list_name': 'OTX_Reputation',
+    'list_url': 'https://reputation.alienvault.com/reputation.generic.gz',
+    'list_format': 'TXT'
+  }
+]
+_THREATLIST_S3_BUCKET = 'myBucket'
+_THREATLIST_S3_KEY_PATH = 'threatlists'  # Without trailing or proceeding slash
+_AWS_PROFILE = ''  # Set to None to not use a profile (default, ec2 metadata, env vars, etc...)
+_AWS_REGIONS = boto3.session.get_available_regions(service_name='guardduty', partition_name='aws')
+
+# logging
+###############################################################################
+logging.basicConfig(level=logging.INFO)
+_logger = logging.getLogger('guardduty_threat-list')
+
+
+# Functions
+###############################################################################
+def _get_logger():
+  _root_logger = logging.getLogger()
+  _root_logger.setLevel(logging.DEBUG)
+  _formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+  _stderr_logger = logging.StreamHandler()
+  _stderr_logger.setFormatter(_formatter)
+  _stderr_logger.setLevel(_STDERR_OUTPUT_LEVEL)
+  _root_logger.addHandler(_stderr_logger)
+  _root_logger.info('Logger to StdErr Setup; root logger disabled')
+  # Disable noisy libraries
+  logging.getLogger("urllib3").setLevel(logging.WARNING)
+  logging.getLogger("botocore").setLevel(logging.WARNING)
+  return _root_logger
+
+
+def _download_and_decompress_threatlist(_threatlist_url):
+  try:
+    _logger.info('Download latest threatlist: {0}'.format(_threatlist_url))
+    _response = requests.get(_threatlist_url, stream=True)
+    _logger.debug('Response: {0}'.format(_response))
+    _compressed_file = BytesIO()
+    _compressed_file.write(_response.raw.data)
+    _compressed_file.seek(0)
+    _decompressed_file = gzip.GzipFile(fileobj=_compressed_file, mode='rb')
+    _threatlist_string = _decompressed_file.read()
+    return _threatlist_string
+  except Exception as err:
+    _print_stacktrace(err)
+    _logger.warning('Something failed downloading threatlist: {0}'.format(_threatlist_url))
+    exit(1)
+
+
+def _upload_threatlist(list_name, threatlist):
+  try:
+    _logger.info('Upload threatlist to S3')
+    _s3_client = _get_aws_client('s3')
+    _response = _s3_client.put_object(
+      Body=threatlist,
+      Bucket=_THREATLIST_S3_BUCKET,
+      Key='{0}/{1}.txt'.format(_THREATLIST_S3_KEY_PATH, list_name)
+    )
+    _logger.debug('Response: {0}'.format(_response))
+    return
+  except Exception as err:
+    _print_stacktrace(err)
+    _logger.warning('Something failed uploading threatlist to s3')
+    exit(1)
+
+
+def _reformat_threatlist(threatlist):
+  try:
+    _logger.info('Reformat threat list to TXT format')
+    _reformated_threatlist = []
+    for _line in threatlist.splitlines():
+      _line_string = _line.decode('utf8').strip()
+      if _line_string[:1] == '#':
+        # Comment
+        continue
+      elif len(_line_string) == 0:
+        # Empty line
+        continue
+      _reformated_threatlist.append(_line_string.split('#')[0].strip())
+
+    return '\n'.join(_reformated_threatlist)
+  except Exception as err:
+    _print_stacktrace(err)
+    _logger.warning('Something failed reformatting threatlist')
+    exit(1)
+
+
+def _refresh_guardduty_threatlist(list_dict, aws_region):
+  try:
+    # expects a dictionary representing the threat list.
+    # {'list_name':_list_name, 'list_url':_list_url, 'list_format':_list_format}
+    _logger.info('Refresh GuardDuty Threatlist - Region {0}'.format(aws_region))
+    _guardduty_client = _get_aws_client('guardduty')
+
+    _response = _guardduty_client.list_detectors(
+      MaxResults=10,
+    )
+    # Each region,account tuple can only have 1 detector, so we grab that.
+    _detectorId = _response['DetectorIds'][0]
+    _response = _guardduty_client.list_threat_intel_sets(
+      DetectorId=_detectorId,
+      MaxResults=10,
+    )
+    if len(_response['ThreatIntelSetIds']) == 0:
+      _logger.warning('No threatlist found in region {0}'.format(aws_region))
+      return
+    elif len(_response['ThreatIntelSetIds']) > len(_THREATLISTS):
+      _logger.warning('Region {0} has more threatlists configured than script defines.'.format(aws_region))
+      return
+    _threatIntelSetId = _response['ThreatIntelSetIds'][0]
+    _location = "https://s3.amazonaws.com/{0}/{1}/{2}.txt".format(_THREATLIST_S3_BUCKET, _THREATLIST_S3_KEY_PATH,
+                                                                  list_dict['list_name'])
+    _revised_threat_set_name = '{0}_{1}'.format(list_dict['list_name'],
+                                                datetime.now().strftime("%Y%b%d_%H%M").upper())
+    _response = _guardduty_client.update_threat_intel_set(
+      Activate=True,
+      DetectorId=_detectorId,
+      Location=_location,
+      Name=_revised_threat_set_name,
+      ThreatIntelSetId=_threatIntelSetId
+    )
+    return
+  except Exception as err:
+    _print_stacktrace(err)
+    _logger.warning('Something failed while updating threatlist')
+    exit(1)
+
+
+def _signal_handler(signal, frame):
+  print("ERROR: SIGINT received.")
+  sys.exit(3)
+
+
+def _get_aws_client(_service_name):
+  try:
+    _logger.info('Get AWS client')
+    _session_args = {
+      'region_name': _AWS_REGIONS
+    }
+    if _AWS_PROFILE is not None:
+      _session_args['profile_name'] = _AWS_PROFILE
+
+    _logger.debug('Session args: {0}'.format(_session_args))
+    _session = boto3.Session(**_session_args)
+    _client = _session.client(service_name=_service_name)
+    _logger.info('AWS client created')
+    return _client
+  except Exception as err:
+    _print_stacktrace(err)
+    _logger.critical('Unknown error getting AWS client')
+    exit(1)
+
+
+def _print_stacktrace(_stacktrace):
+  if _PRINT_STACKTRACE_ON_ERROR:
+    traceback.print_exc(file=sys.stderr)
+
+
+# Main
+###############################################################################
+def _main():
+  _logger.info('Begin main')
+  for _list_dict in _THREATLISTS:
+    _logger.info('Threat List: {0}'.format(_list_dict))
+    _threatlist = _download_and_decompress_threatlist(_list_dict['list_url'])
+    _threatlist = _reformat_threatlist(_threatlist)
+    _upload_threatlist(_list_dict['list_name'], _threatlist)
+
+  _logger.info('Refresh AWS GuardDuty to newly uploaded threat list')
+  for _aws_region in _AWS_REGIONS:
+    for _list_dict in _THREATLISTS:
+      _logger.info('AWS Region: {0}  Threat List: {1}'.format(_aws_region, _list_dict))
+      _refresh_guardduty_threatlist(_list_dict, _aws_region)
+  _logger.info('Finish main')
+  return
+
+
+if __name__ == "__main__":
+  try:
+    signal.signal(signal.SIGINT, _signal_handler)
+    _logger = _get_logger()
+    _main()
+  except Exception as err:
+    _print_stacktrace(err)
+    _logger.critical('Unknown error in main')
+    exit(1)
